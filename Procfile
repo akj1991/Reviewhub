128217ad9cd21b22e72748f91dd7bc165880fcec
akj1991 <akhilonattu@gmail.com>
2022-03-23 11:20:21 +0530

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn reviewhub.wsgi --log-file -
--- reviewhub/settings.py
+++ reviewhub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6-g#f-xufmw81dxo4yvs+^nu##iuz7&@=#ak%5&q6(4&mz$5qr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
